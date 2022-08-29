fe49692f1977ef8490bdf51d94b30bc1cf1fb50b
Prashant Kumar <108957783+PK4878@users.noreply.github.com>
2022-08-29 23:50:36 +0530

    Food menu

---
+++ Food_Menu.py
@@ -0,0 +1,17 @@
+def menu():
+    print("Welcome,\n 1.Tandoori Chicken\n 2.Burger\n 3.cake\n")
+    option=input()
+
+    if option=="1":
+        print("Tandoori Chicken")
+
+    if option=="2":
+        print("Burger")
+
+    if option=="3":
+        print("cake")
+
+    
+menu()
+
+
